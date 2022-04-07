36a7a8b842c3a45d3bbefef684c8ad6ded0b6acb
Dmitry Sharabin <dmitrysharabin@gmail.com>
2022-04-07 22:24:44 +0300

    First pass

---
+++ mavo-share.js
@@ -0,0 +1,60 @@
+(function () {
+	Mavo.Plugins.register("share", {});
+
+	Mavo.Actions.Functions.share = async function (text, url, title) {
+		if (!("share" in navigator)) {
+			Mavo.warn("Your browser doesn't support sharing.");
+			return;
+		}
+
+		[text, url, title] = [text, url, title].map(Mavo.value);
+
+		if (!text && !url) {
+			// Nothing to share
+			return;
+		}
+
+		if (!text) {
+			[text, url] = [url];
+		}
+
+		// What if text is a string representation of a URL?
+		try {
+			const tempURL = new URL(text);
+
+			// Case: share(url[, title])
+			if (url && !title) {
+				title = url;
+			}
+
+			[url, text] = [tempURL.href];
+		} catch {}
+
+		if (url) {
+			try {
+				url = new URL(url);
+
+				// Case: share(text, url[, title])
+				url = url.href;
+			} catch {
+				// Case: share(text, title)
+				[title, url] = [url];
+			}
+		}
+
+		const data = {
+			text,
+			url,
+			title
+		};
+
+		try {
+			await navigator.share(data);
+		} catch (error) {
+			const title = `Oops! 😳 We couldn't share your data. Something went wrong: ${error.message}`;
+			const message = `Data was: ${JSON.stringify(data)}`;
+
+			console.info(`%c${title}`, "color: #c04; font-weight: bold;", message);
+		}
+	};
+})();
